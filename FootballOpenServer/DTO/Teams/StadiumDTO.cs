36ae20a017dce0596d2d944fa62a927036a2ceee
Tom Papaioannou <tom-papaioannou@outlook.com>
2026-04-18 21:07:54 +0300

    Add cities and team information dto classes (#23)

---
+++ FootballOpenServer/DTO/Teams/StadiumDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FootballOpenServer.DTO.Teams
+{
+    public class StadiumDTO
+    {
+        public Guid StadiumID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public double Latitude { get; set; } = 0.0;
+        public double Longitude { get; set; } = 0.0;
+    }
+}
