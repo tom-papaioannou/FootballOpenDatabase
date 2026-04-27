92007955308216d332189cb80fd7389b9f3d0ea1
Tom Papaioannou <tom-papaioannou@outlook.com>
2026-04-27 19:43:41 +0300

    Add team shirt numbers on contracts of players

---
+++ FootballOpenServer/Migrations/20260427163930_AddShirtNumbers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballOpenServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShirtNumbers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "ShirtNumber",
+                table: "Contracts",
+                type: "tinyint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShirtNumber",
+                table: "Contracts");
+        }
+    }
+}
