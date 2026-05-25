ee6b613e13060856f6e5bd84bbb917345530c3a1
Tom Papaioannou <tom-papaioannou@outlook.com>
2026-05-25 22:00:27 +0300

    Add weight and height to person (#48)

---
+++ FootballOpenServer/Migrations/20260525185227_AddWeightAndHeightOnPerson.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballOpenServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWeightAndHeightOnPerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "People",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Weight",
+                table: "People",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "People");
+        }
+    }
+}
