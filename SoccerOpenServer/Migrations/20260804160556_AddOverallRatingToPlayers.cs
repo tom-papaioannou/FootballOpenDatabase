7c5e814281a567a157ee60deb30e41f6fd55ba4c
Tom Papaioannou <tom-papaioannou@outlook.com>
2026-08-04 19:07:48 +0300

    Add an overall rating to each player (#79)

---
+++ SoccerOpenServer/Migrations/20260804160556_AddOverallRatingToPlayers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoccerOpenServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOverallRatingToPlayers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Rating",
+                table: "PlayerStats",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "PlayerStats");
+        }
+    }
+}
