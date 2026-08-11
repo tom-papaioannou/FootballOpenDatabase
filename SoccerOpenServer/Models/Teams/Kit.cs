b5b804312c1d6acc5fc1145c2c7c137b89c21088
Tom Papaioannou <tom-papaioannou@outlook.com>
2026-08-11 21:37:58 +0300

    Add kit shapes (#80)

--- SoccerOpenServer/Models/Teams/Kit.cs
+++ SoccerOpenServer/Models/Teams/Kit.cs
@@ -12,6 +12,7 @@ namespace SoccerOpenServer.Models.Teams
         public string HomeShortsColor { get; set; } = string.Empty;
         public string AwayShirtColor { get; set; } = string.Empty;
         public string AwayShortsColor { get; set; } = string.Empty;
+        public KitShapeEnum KitShape { get; set; } = KitShapeEnum.Empty;
 
         [JsonIgnore]
         public virtual Team? Team { get; set; }
